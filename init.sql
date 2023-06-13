87fe9bc9fb7b7c4e03b94a522e91d55a48a82fcb
Paul <paulmartinez@javerianacali.edu.co>
2023-06-13 14:15:10 -0500

    initial db setup w/o entregas

---
+++ init.sql
@@ -0,0 +1,60 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS "user" (
+    userId UUID PRIMARY KEY NOT NULL,
+    personalId INTEGER NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    passwordHash VARCHAR(100) NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    lastName VARCHAR(25) NOT NULL,
+    estudiante BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS estado (
+    estadoId INTEGER PRIMARY KEY NOT NULL,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS anteproyecto (
+    anteproyectoId UUID PRIMARY KEY NOT NULL,
+    noRadicacion INTEGER NOT NULL,
+    titulo VARCHAR(100) NOT NULL,
+    fechaEntregaAEvaluador DATE,
+    fechaEntregaDeEvaluador DATE,
+    estado INTEGER NOT NULL,
+    FOREIGN KEY (estado) REFERENCES estado(estadoId)
+);
+
+CREATE TABLE IF NOT EXISTS autores (
+    autoresId UUID PRIMARY KEY NOT NULL,
+    anteproyectoId UUID NOT NULL,
+    userId UUID NOT NULL,
+    FOREIGN KEY (anteproyectoId) REFERENCES anteproyecto(anteproyectoId),
+    FOREIGN KEY (userId) REFERENCES "user"(userId)
+);
+
+CREATE TABLE IF NOT EXISTS "role" (
+    roleId INTEGER PRIMARY KEY NOT NULL,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS evaluadores (
+    evaluadoresId UUID PRIMARY KEY NOT NULL,
+    anteproyectoId UUID NOT NULL,
+    userId UUID NOT NULL,
+    roleId INTEGER NOT NULL,
+    FOREIGN KEY (anteproyectoId) REFERENCES anteproyecto(anteproyectoId),
+    FOREIGN KEY (userId) REFERENCES "user"(userId),
+    FOREIGN KEY (roleId) REFERENCES "role"(roleId)
+);
+
+CREATE TABLE IF NOT EXISTS proyectogrado (
+    proyectoId UUID PRIMARY KEY NOT NULL,
+    anteproyectoId UUID NOT NULL,
+    fechaSustentacion DATE,
+    notaDefinitiva NUMERIC(3,2) CHECK (notaDefinitiva BETWEEN 0.00 AND 5.00),
+    nroActa INTEGER,
+    mencionHonor BOOLEAN,
+    gradoPostulacion VARCHAR(20) DEFAULT 'NO SE QUE VA ACA',
+    entregaDocs BOOLEAN
+);
