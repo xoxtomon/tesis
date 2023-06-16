16f405b1e9a744b2f22399ba77a1cd58b37c7f5b
Paul <xoxtomon@hotmail.com>
2023-06-16 13:56:11 -0500

    added jwt dependencies to pom.xml

--- backend/src/main/java/tesis/backend/backend/security/config/JwtService.java
+++ backend/src/main/java/tesis/backend/backend/security/config/JwtService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtService {
 
-    public String extractUsername(String jwt) {
+    public String extractUsername(String token) {
         return null;
     }
 
