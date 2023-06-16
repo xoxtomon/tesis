4c326958be8f6a8be1e4145c4581082214c94bd6
Paul <xoxtomon@hotmail.com>
2023-06-16 15:23:56 -0500

    working on jwtservice

--- backend/src/main/java/tesis/backend/backend/security/config/JwtService.java
+++ backend/src/main/java/tesis/backend/backend/security/config/JwtService.java
@@ -1,12 +1,34 @@
 package tesis.backend.backend.security.config;
 
+import java.security.Key;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoder;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+        .parserBuilder()
+        .setSigningKey(getSigningKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
