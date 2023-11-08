dca6ae9bcde0098af7a14e094f5d626b1639dc46
Paul <xoxtomon@hotmail.com>
2023-11-08 16:09:13 -0500

    deleted EvaluadorKey dependencies

--- src/main/java/tesis/backend/backend/evaluador/repository/EvaluadorRepository.java
+++ src/main/java/tesis/backend/backend/evaluador/repository/EvaluadorRepository.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tesis.backend.backend.evaluador.entity.Evaluador;
-import tesis.backend.backend.evaluador.entity.EvaluadorKey;
 
 public interface EvaluadorRepository extends JpaRepository<Evaluador, UUID>{
     
