9a02da441b157b554b0ef2b200c4e556dfa5bc32
Paul <paulmartinez@javerianacali.edu.co>
2023-11-09 18:10:52 -0500

    Changed constructor to support evaluadores and autores on creation

--- src/main/java/tesis/backend/backend/anteproyecto/entity/Anteproyecto.java
+++ src/main/java/tesis/backend/backend/anteproyecto/entity/Anteproyecto.java
@@ -1,6 +1,8 @@
 package tesis.backend.backend.anteproyecto.entity;
 
 import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -12,7 +14,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -44,9 +45,15 @@ public class Anteproyecto {
 
     @Column(name = "fechacreacion")
     private Date fechaCreacion;
+    
+    @Column(name = "fecha_aprobacion")
+    private Date fechaAprobacion;
 
     @Column(name = "estado")
     private Integer estado;
+
+    @Column(name = "nroentrega")
+    private Integer nroEntrega = 0;
 
     @OneToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "autores", joinColumns = @JoinColumn(name = "anteproyectoid"), inverseJoinColumns = @JoinColumn(name = "userid"))
@@ -61,20 +68,32 @@ public class Anteproyecto {
     @OneToMany(mappedBy = "anteproyectoId")
     private Set<Evaluador> evaluadores;
 
-    public Anteproyecto(Integer noRadicacion, String titulo, Date fechaEntregaAEvaluador, Date fechaEntregaDeEvaluador,
-            Date fechaCreacion, Integer estado) {
+    /* public Anteproyecto(Integer noRadicacion, String titulo, Date fechaEntregaAEvaluador, Date fechaEntregaDeEvaluador,
+            Date fechaCreacion, Date fechaAprobacion, Integer estado) {
         this.noRadicacion = noRadicacion;
         this.titulo = titulo;
         this.fechaEntregaAEvaluador = fechaEntregaAEvaluador;
         this.fechaEntregaDeEvaluador = fechaEntregaDeEvaluador;
         this.fechaCreacion = fechaCreacion;
+        this.fechaAprobacion = fechaAprobacion;
         this.estado = estado;
+    } */
+    
+    public Anteproyecto(Integer nroRadicacion, String titulo, List<String> autores, List<Evaluador> evaluadores) {
+        this.noRadicacion = nroRadicacion;
+        this.titulo = titulo;
+        this.estado = 3;
+
+        LocalDate today = LocalDate.now();
+        this.fechaCreacion = Date.valueOf(today);
     }
+
 
     public Anteproyecto(Integer noRadicacion, String titulo, Integer estado) {
         this.noRadicacion = noRadicacion;
         this.titulo = titulo;
         this.estado = estado;
+        //this.fechaCreacion = date.today TODO
     }
 
 }
