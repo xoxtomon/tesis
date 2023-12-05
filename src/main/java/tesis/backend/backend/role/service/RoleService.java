7ca33b1c6b0d5926373201bc73731f0833a9b277
Paul <paulmartinez@javerianacali.edu.co>
2023-12-05 14:46:21 -0500

    cleaned role service

--- src/main/java/tesis/backend/backend/role/service/RoleService.java
+++ src/main/java/tesis/backend/backend/role/service/RoleService.java
@@ -11,10 +11,4 @@ public class RoleService {
     @Autowired
     RoleRepository roleRepository;
 
-// Used on registration to ensure role 1 is not assigned to any other user than admin
-    public List<Integer> PruneAdminRole(List<Integer> roleIds) {
-        if(roleIds.contains(1)){roleIds.remove(Integer.valueOf(1));}
-        return roleIds;
-    }
-
 }
